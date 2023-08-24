9075b0315daad9615ed924568163bf5c6be21e88
fuodorov <fuodorov1998@gmail.com>
2023-08-24 21:49:35 +0700

    Update actions

--- redpic/__init__.py
+++ redpic/__init__.py
@@ -8,7 +8,7 @@ from .beam import *
 from .constants import *
 from .solver import *
 
-__version__ = "0.7.12"
+__version__ = "0.7.13"
 __doc__ = """Relativictic Difference Scheme Particle-in-Cell code (REDPIC)"""
 
 __all__ = constants.__all__ + beam.__all__ + solver.__all__ + accelerator.__all__
