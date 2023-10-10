22484df71c226aba06e844ab63b425163775e837
Vyacheslav Fedorov <fuodorov1998@gmail.com>
2023-10-10 05:05:49 +0000

    Update cuda implementation

--- redpic/utils/jit.py
+++ redpic/utils/jit.py
@@ -1,5 +1,4 @@
 from numba import jit as numba_jit
-from numba.cuda import jit as numba_cuda_jit
 
 from redpic.core import config as cfg
 
@@ -8,14 +7,6 @@ def jit(func):
     def wrapper(*args, **kwargs):
         if cfg.DISABLE_JIT:
             return func(*args, **kwargs)
-
-        if cfg.ENABLE_CUDA:
-            return numba_cuda_jit(
-                fastmath=not cfg.DISABLE_FASTMATH,
-                cache=not cfg.DISABLE_CACHE,
-            )(
-                func
-            )(*args, **kwargs)
 
         return numba_jit(
             nopython=not cfg.DISABLE_NOPYTHON,
